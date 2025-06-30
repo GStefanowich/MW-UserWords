e4a48045c37e7b24d6048779482a3a8fea53e9aa
7873930+GStefanowich <7873930+GStefanowich@users.noreply.github.com>
2025-06-30 13:05:30 -0400

    style: Merged similar classes

--- includes/Hooks/Variables.php
+++ includes/Hooks/Variables.php
@@ -3,9 +3,24 @@
 namespace MediaWiki\Extension\UserWords\Hooks;
 
 use MediaWiki\Extension\UserWords\UserWords;
-use MediaWiki\Hook\GetMagicVariableIDsHook;
 
-class Variables implements GetMagicVariableIDsHook {
+class Variables implements
+    \MediaWiki\Hook\ParserGetVariableValueSwitchHook,
+    \MediaWiki\Hook\GetMagicVariableIDsHook
+{
+    private readonly array $variables;
+
+    public function __construct(
+        private readonly UserWords $magicWords
+    ) {
+        $this->variables = [
+            UserWords::MAGIC_USER_FIRST_REVISION,
+            UserWords::MAGIC_USER_GROUPS,
+            UserWords::MAGIC_USER_REGISTRATION,
+            UserWords::MAGIC_USER_LANGUAGE_CODE,
+        ];
+    }
+
     /**
      * @inheritdoc
      */
@@ -13,12 +28,41 @@ class Variables implements GetMagicVariableIDsHook {
         $variableIDs = [
             // Spread the original words
             ...$variableIDs,
-            
+
             // Add all of our words
-            UserWords::MAGIC_USER_FIRST_REVISION,
-            UserWords::MAGIC_USER_GROUPS,
-            UserWords::MAGIC_USER_REGISTRATION,
-            UserWords::MAGIC_USER_LANGUAGE_CODE,
+            ...$this->variables,
         ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function onParserGetVariableValueSwitch( $parser, &$variableCache, $magicWordId, &$ret, $frame ): void {
+        $user = $this->magicWords->getUser($parser->getPage());
+        if ( !$user ) {
+            return;
+        }
+
+        switch ( $magicWordId ) {
+            case UserWords::MAGIC_USER_REGISTRATION: {
+                $parser->incrementExpensiveFunctionCount();
+                $ret = $variableCache[$magicWordId] = $this->magicWords->getUserRegistration($parser, $user);
+                break;
+            }
+            case UserWords::MAGIC_USER_GROUPS: {
+                $parser->incrementExpensiveFunctionCount();
+                $ret = $variableCache[$magicWordId] = $this->magicWords->getUserGroups($parser, $user, false);
+                break;
+            }
+            case UserWords::MAGIC_USER_FIRST_REVISION: {
+                $parser->incrementExpensiveFunctionCount();
+                $ret = $variableCache[$magicWordId] = $this->magicWords->getUserFirstRevision($parser, $user);
+                break;
+            }
+            case UserWords::MAGIC_USER_LANGUAGE_CODE: {
+                $ret = $variableCache[$magicWordId] = $this->magicWords->getUserLanguageCode($parser, $user);
+                break;
+            }
+        }
     }
 }
