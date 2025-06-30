71224334933e0a9100e78100134f9af083c02d25
7873930+GStefanowich <7873930+GStefanowich@users.noreply.github.com>
2025-06-30 13:12:42 -0400

    fix: Move expensive function incrementer   Handles the bool val now   Called before fetching the user

--- includes/Hooks/Variables.php
+++ includes/Hooks/Variables.php
@@ -38,31 +38,29 @@ class Variables implements
      * @inheritdoc
      */
     public function onParserGetVariableValueSwitch( $parser, &$variableCache, $magicWordId, &$ret, $frame ): void {
-        $user = $this->magicWords->getUser($parser->getPage());
-        if ( !$user ) {
+        if ( !in_array($magicWordId, $this->variables) ) {
             return;
         }
 
-        switch ( $magicWordId ) {
-            case UserWords::MAGIC_USER_REGISTRATION: {
-                $parser->incrementExpensiveFunctionCount();
-                $ret = $variableCache[$magicWordId] = $this->magicWords->getUserRegistration($parser, $user);
-                break;
-            }
-            case UserWords::MAGIC_USER_GROUPS: {
-                $parser->incrementExpensiveFunctionCount();
-                $ret = $variableCache[$magicWordId] = $this->magicWords->getUserGroups($parser, $user, false);
-                break;
-            }
-            case UserWords::MAGIC_USER_FIRST_REVISION: {
-                $parser->incrementExpensiveFunctionCount();
-                $ret = $variableCache[$magicWordId] = $this->magicWords->getUserFirstRevision($parser, $user);
-                break;
-            }
-            case UserWords::MAGIC_USER_LANGUAGE_CODE: {
-                $ret = $variableCache[$magicWordId] = $this->magicWords->getUserLanguageCode($parser, $user);
-                break;
-            }
+        // Increment the expensive parser count
+        if ( !$parser->incrementExpensiveFunctionCount() ) {
+            $ret = $variableCache[$magicWordId] = '';
+            return;
         }
+
+        // Try getting the user from the current page
+        $user = $this->magicWords->getUser($parser->getPage());
+        if ( !$user ) {
+            $ret = $variableCache[$magicWordId] = '';
+            return;
+        }
+
+        $ret = $variableCache[$magicWordId] = match ( $magicWordId ) {
+            UserWords::MAGIC_USER_REGISTRATION => $this->magicWords->getUserRegistration($parser, $user),
+            UserWords::MAGIC_USER_GROUPS => $this->magicWords->getUserGroups($parser, $user, false),
+            UserWords::MAGIC_USER_FIRST_REVISION => $this->magicWords->getUserFirstRevision($parser, $user),
+            UserWords::MAGIC_USER_LANGUAGE_CODE => $this->magicWords->getUserLanguageCode($parser, $user),
+            default => '',
+        };
     }
 }
